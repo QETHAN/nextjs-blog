f9b8112f116afd0658b759073e504b472cf0d76d
QETHAN <qbylucky@gmail.com>
2024-03-11 10:52:09 +0800

    add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return <footer className="py-5">
+        <div className="text-center">
+            <a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2024052248号</a>
+        </div>
+    </footer>
+}
